7982052e560c6a4a779920d91cbbb26c48b83c05
Sarika Matthew <pragmattika@Sarika-Matthews-MacBook-Pro-2.local>
2017-11-17 23:35:33 -0500

    guessed letters and win count need to be fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,6 +14,12 @@ var message = {
 var wordRandom;
 var newWord;
 var blankWord = [];
+var guess = 0;
+var isLetter = false;
+var alreadyGuess = false;
+var lettersGuess = [];
+var correctLetter = false;
+var newWinner = true;
 
 
 
@@ -58,36 +64,106 @@ document.onkeyup = function(event) {
 
 //check for letter
 
-//     for (var i = 0; i < alphabet.length; i++) {
-//         if (userGuess === alphabet.charAt(i)) {
-//             isLetter = true;
-//         }
-//     }
+    for (var i = 0; i < alphabet.length; i++) {
+        if (userGuess === alphabet.charAt(i)) {
+            isLetter = true;
+        }
+    }
 
-//     if (isLetter == false && enter != 13) {
-//         $('#notices').html(notices.validLetter);
-//     }
+    if (isLetter == false && enter != 13) {
+        $('#message').html(message.needLetter);
+    }
 
-// //letter already guessed
+    
 
-// for (var i = 0; i < lettersGuessed.length; i++) {
-//         if (userGuess === lettersGuessed[i]) {
-//             alreadyGuessed = true;
-//         }
-//     }
-// }
+//letter already guessed
+
+for (var i = 0; i < lettersGuess.length; i++) {
+        if (userGuess === lettersGuess[i]) {
+            alreadyGuess = true;
+        }
+    }
+
+for (var i = 0; i < blankWord.length; i++) {
+        if (userGuess == blankWord[i]) {
+            alreadyGuess = true;
+        }
+    }
+
+    if (alreadyGuess == true) {
+        $('#message').html(message.repeatGuess);
+    }
+
 
 //check letter - if correct - insert
 
+for (var i = 0; i < newWord.length; i++) {
+        if (newWord[i] === userGuess) {
+            blankWord[i] = newWord[i];
+            correctLetter = true;
+        }
+    }
+
+
+
 //guessed letters into array
+
+if (isLetter == true && alreadyGuess == false && correctLetter == false) {
+        lettersGuess.push(userGuess);
+        lives--;
+        $('#lives').html(lives);
+    }
+
+    $('#lettersGuess').html(lettersGuess);
+    $('#blankWord').html(blankWord);
 
 //take away guess for wrong letter
 
-//if you lose, loss counter goes up
+if (lettersGuess.length == 10) {
+        $('#message').html(message.loss);
+        lettersGuess = [];
+        $('#lettersGuess').html(lettersGuess);
+        //if you lose, loss counter goes up
+
+        lossCount++;
+        $('#lossCount').html(lossCount);
+        lives = 10;
+        newGame();
+    }
+
+
+
 
 //reset after key
 
+correctLetter = false;
+isLetter = false;
+alreadyGuess = false;
+
 //user wins
+
+function win() {
+
+    for (var i = 0; i < newWord.length; i++) {
+        if (blankWord[i] == " _ ") {
+            newWinner = false;
+        }
+    }
+
+    if (newWinner == true) {
+        $('#message').html(message.win);
+        winCount++;
+        $('#winCount').html(winCount);
+        lettersGuess = [];
+        $('#lettersGuess').html(lettersGuess);
+        lives = 10;
+        $('#lives').html(lives);
+
+        newGame();
+    }
+
+    newWinner = true;
+}
 }
 
 
